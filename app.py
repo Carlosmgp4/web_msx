22c5b5d44ae3fee91531556fac890765577ba431
Carlosmgp4 <carlosmanuelgamezperezdeguzman@gmail.com>
2021-05-20 16:27:35 +0200

    He realizado dentro del app.py las funciones que llaman al resto de webs y he creado la cual realizara la busqueda, el resto estoy realizando pruebas para ver la forma en la que se lee el fichero json pero no es la forma definitiva.

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, url_for, render_template,request, abort
+import json
 app = Flask(__name__)
+
+with open ("msx.json") as f:
+    datos = json.load(f)
 
 @app.route("/")
 def index():
     return render_template("inicio.html")
 
+@app.route("/juegos",methods=["GET","POST"])
+def juegos():
+    return render_template("juegos.html")
 
+@app.route("/listajuegos",methods=["POST"])
+def listajuegos():
+    try:
+        bus=request.form["nombrebus"]
+    except:
+        abort(404)
+    for w in datos:
+        if bus in w.get("nombre"):
+            return render_template("listajuegos.html")
 
 app.run("0.0.0.0",5000,debug=True)
